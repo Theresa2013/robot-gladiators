f947af9088d90a352ba944a3ed790cee7db88bf8
Your Name <you@example.com>
2022-04-13 11:00:50 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,23 +3,27 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+//Game States
+//"WIN" - Player robot has defeated all enemy-robots
+//  *Fight all enemy-robots    
+//  *Defeat each enemy-robot
+//"LOSE" - Player robot's health is zero or less
+
+var fight = function (enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+    
+    //ask player if they'd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     //if the player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
         //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
         enemyHealth = enemyHealth - playerAttack;
-
         //Log a resulting message to the console so we know that it worked.
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -35,7 +39,6 @@ var fight = function () {
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
-
         // Log a resulting message to the console so we know that it worked.
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -68,4 +71,7 @@ var fight = function () {
     }
 };
 
-fight();
+//run function to start the fight
+for(var i = 0;i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
